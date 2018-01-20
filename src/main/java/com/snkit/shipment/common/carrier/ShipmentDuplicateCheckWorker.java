f025d5614443aab9c53fcd06736bf670f079195f
Kollu Sreenivasa Rao <kollu.srinu@gmail.com>
2018-01-20 14:24:12 +0530

    new system out added

--- src/main/java/com/snkit/shipment/common/carrier/ShipmentDuplicateCheckWorker.java
+++ src/main/java/com/snkit/shipment/common/carrier/ShipmentDuplicateCheckWorker.java
@@ -7,7 +7,7 @@ public class ShipmentDuplicateCheckWorker extends ShipmentWorker {
 	@Override
 	public void execute(ShipmentContext context) {
 		System.out.println(" From ShipmentDuplicateCheckWorker "+context.getName());
-		
+		System.out.println(" From ShipmentDuplicateCheckWorker "+context.getName());
 
 	}
 
